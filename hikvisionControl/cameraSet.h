589d5cb6c100e549ff98737c25f44ff09fd9693d
여지윤 <kkachi1025@gmail.com>
2025-01-03 16:56:35 +0900

    "feat: 카메라 설정 클래스 추가 및 주요 함수 수정"
    `cameraSet.cpp`와 `cameraSet.h` 파일이 추가되어 카메라 설정을 위한 `cameraSet` 클래스가 정의되었습니다. 이 클래스는 로그인 정보, 장치 정보, 사용자 ID를 관리하며, 로그인 및 설정 관련 메서드를 포함하고 있습니다.
    
    `hikvisionControl.cpp` 파일에서 초기화 함수, 윈도우 생성 함수, 로그인 함수, 라이브 뷰 설정 함수, 녹화 시작 및 종료 함수, 윈도우 메시지 루프 함수, 키 입력 확인 함수 등이 추가되었습니다. 또한, `main` 함수가 수정되어 프로그램의 흐름이 변경되었습니다.
    
    `hikvisionControl.vcxproj` 및 `hikvisionControl.vcxproj.filters` 파일에 `cameraSet.cpp`와 `cameraSet.h` 파일이 포함되도록 수정되었습니다.

---
+++ hikvisionControl/cameraSet.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include "Windows.h"
+#include "HCNetSDK.h"
+#include <time.h>
+#pragma comment(lib, "HCNetSDK.lib")
+#pragma warning(disable:4996)
+
+using namespace std;
+
+class cameraSet
+{
+private:
+	NET_DVR_USER_LOGIN_INFO loginInfo;
+	NET_DVR_DEVICEINFO_V40 deviceInfo;
+	LONG lUserID;
+
+public:
+	cameraSet(NET_DVR_USER_LOGIN_INFO* loginInfo, NET_DVR_DEVICEINFO_V40* deviceInfo, LONG* lUserID)
+	{
+		memset(&this->loginInfo, 0, sizeof(this->loginInfo));
+		memset(&this->deviceInfo, 0, sizeof(this->deviceInfo));
+		this->loginInfo = *loginInfo;
+		this->deviceInfo = *deviceInfo;
+		this->lUserID = *lUserID;
+	}
+
+	void setLoginInfo(const char* ip, const char* id, const char* pw, int port)
+	{
+		memset(&loginInfo, 0, sizeof(loginInfo));
+		strcpy(loginInfo.sDeviceAddress, ip);
+		strcpy(loginInfo.sUserName, id);
+		strcpy(loginInfo.sPassword, pw);
+		loginInfo.wPort = port;
+		loginInfo.bUseAsynLogin = 0;
+	}
+
+	void setIUserID()
+	{
+		lUserID = NET_DVR_Login_V40(&loginInfo, &deviceInfo);
+		if (lUserID < 0)
+		{
+			cerr << "NET_DVR_Login_V40 error!" << endl;
+			NET_DVR_Cleanup();
+		}
+		else cout << "NET_DVR_Login_V40 success!" << endl;
+	}
+
+	NET_DVR_USER_LOGIN_INFO getLoginInfo()
+	{
+		return loginInfo;
+	}
+
+	NET_DVR_DEVICEINFO_V40 getDeviceInfo()
+	{
+		return deviceInfo;
+	}
+
+	LONG getIUserID()
+	{
+		return lUserID;
+	}
+};
--- hikvisionControl/hikvisionControl.cpp
+++ hikvisionControl/hikvisionControl.cpp
@@ -4,38 +4,149 @@
 #include "Windows.h"
 #include "HCNetSDK.h"
 #include <time.h>
+#include <conio.h>
+#include "cameraSet.h"
+#include <thread>
 #pragma comment(lib, "HCNetSDK.lib")
 #pragma warning(disable:4996)
 
 using namespace std;
 
-int main()
-{
-	//---------------------------------------
+bool isRecording = false;
 
+//초기화 함수
+int init()
+{
 	if (!NET_DVR_Init())
 	{
 		cerr << "NET_DVR_Init error!" << endl;
 		return -1;
 	}
 	cout << "NET_DVR_Init success!" << endl;
+}
 
-	//---------------------------------------
-	//윈도우 생성
+//윈도우 생성 함수
+HWND createWindow()
+{
 	HWND hwnd = CreateWindowEx(
-		0, 
-		L"STATIC", 
-		L"Hikvision Live View", 
+		0,
+		L"STATIC",
+		L"Hikvision Live View",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100, 
-		100, 
-		800, 
-		600, 
-		nullptr, 
-		nullptr, 
-		GetModuleHandle(nullptr), 
+		100,
+		100,
+		800,
+		600,
+		nullptr,
+		nullptr,
+		GetModuleHandle(nullptr),
 		nullptr
 	);
+
+	if (!hwnd)
+	{
+		cerr << "CreateWindowEx error!" << endl;
+		NET_DVR_Cleanup();
+		return nullptr;
+	}
+
+	return hwnd;
+}
+
+//로그인 함수
+cameraSet logIn(const char* ip, const char* id, const char* pw, int port)
+{
+	NET_DVR_USER_LOGIN_INFO loginInfo = { 0 };
+	NET_DVR_DEVICEINFO_V40 deviceInfo = { 0 };
+	LONG lUserID = { 0 };
+
+	cameraSet cam = cameraSet(&loginInfo, &deviceInfo, &lUserID);
+
+	cam.setLoginInfo(ip, id, pw, port);
+	cam.setIUserID();
+
+	return cam;
+}
+
+//라이브 뷰 설정 함수
+NET_DVR_PREVIEWINFO setPreviewInfo(int lChannel, int dwStreamType, int dwLinkMode, HWND hwnd) //클래스?
+{
+	NET_DVR_PREVIEWINFO previewInfo = { 0 };
+	previewInfo.lChannel = lChannel;
+	previewInfo.dwStreamType = dwStreamType;
+	previewInfo.dwLinkMode = dwLinkMode;
+	previewInfo.hPlayWnd = hwnd; //영상 출력할 윈도우 핸들
+	return previewInfo;
+}
+
+//녹화 시작 함수
+void StartRecording(LONG realPlayHandle, char* filePath) {
+	if (!NET_DVR_SaveRealData(realPlayHandle, filePath)) {
+		cout << "Failed to start saving data, error: " << NET_DVR_GetLastError() << endl;
+	}
+	else {
+		cout << "Recording started. Saving to: " << filePath << endl;
+		isRecording = true;
+	}
+}
+
+//녹화 종료 함수
+void StopRecording(LONG realPlayHandle) {
+	if (!NET_DVR_StopSaveRealData(realPlayHandle)) {
+		cout << "Failed to stop saving data, error: " << NET_DVR_GetLastError() << endl;
+	}
+	else {
+		cout << "Recording stopped." << endl;
+		isRecording = false;
+	}
+}
+
+//윈도우 메시지 루프 함수
+void showWindow()
+{
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
+//키 입력 확인 함수
+void checkKey(LONG playHandle, char* saveFilePath)
+{
+	while (true)
+	{
+		if (_kbhit())
+		{
+			char ch = _getch();
+			if (ch == 'R' || ch == 'r')
+			{
+				if (isRecording)
+				{
+					StopRecording(playHandle);
+				}
+				else
+				{
+					StartRecording(playHandle, saveFilePath);
+				}
+			}
+			else if (ch == 'Q' || ch == 'q')
+			{
+				break;
+			}
+		}
+		Sleep(100); // CPU 사용률 절약
+	}
+}
+
+int main()
+{
+	if (!init()) return -1;
+
+	cameraSet cam = logIn("192.168.1.100", "admin", "12345", 8000);
+
+	HWND hwnd = createWindow();
 	if (!hwnd)
 	{
 		cerr << "CreateWindowEx error!" << endl;
@@ -43,53 +154,35 @@ int main()
 		return -1;
 	}
 
-	//---------------------------------------
-	//로그인 정보 설정
+	NET_DVR_PREVIEWINFO previewInfo = setPreviewInfo(1, 0, 0, hwnd);
 
-	NET_DVR_USER_LOGIN_INFO loginInfo = { 0 };
-	NET_DVR_DEVICEINFO_V40 deviceInfo = { 0 };
-
-	strncpy(loginInfo.sDeviceAddress, "192.168.1.100", sizeof(loginInfo.sDeviceAddress) - 1); //디바이스 IP
-	loginInfo.wPort = 8000; //포트 번호
-	strncpy(loginInfo.sUserName, "admin", sizeof(loginInfo.sUserName) - 1); //사용자 이름
-	strncpy(loginInfo.sPassword, "12345", sizeof(loginInfo.sPassword) - 1); //비밀번호
-
-	LONG lUserID = NET_DVR_Login_V40(&loginInfo, &deviceInfo);
-	if (lUserID < 0) {
-		std::cerr << "로그인 실패, 오류 코드: " << NET_DVR_GetLastError() << std::endl;
-		NET_DVR_Cleanup();
-		return -1;
-	}
-
-	//---------------------------------------
-	//영상 출력 설정
-
-	NET_DVR_PREVIEWINFO previewInfo = { 0 };
-	previewInfo.lChannel = 1;
-	previewInfo.dwStreamType = 0;
-	previewInfo.dwLinkMode = 0;
-	previewInfo.hPlayWnd = hwnd; //영상 출력할 윈도우 핸들
-
-	LONG playHandle = NET_DVR_RealPlay_V40(lUserID, &previewInfo, nullptr, nullptr);
+	LONG playHandle = NET_DVR_RealPlay_V40(cam.getIUserID(), &previewInfo, nullptr, nullptr);
 	if (playHandle < 0) {
-		std::cerr << "라이브 뷰 시작 실패, 오류 코드: " << NET_DVR_GetLastError() << std::endl;
+		cerr << "라이브 뷰 시작 실패, 오류 코드: " << NET_DVR_GetLastError() << endl;
+		//NET_DVR_Cleanup();
+		//return -1;
 	}
 	else {
-		std::cout << "라이브 뷰 시작 성공" << std::endl;
+		cout << "라이브 뷰 시작 성공" << endl;
 	}
+	
+	//---------------------------------------
+	//메시지 루프 (라이브 스트리밍 유지)
 
-	// 메시지 루프 (라이브 스트리밍 유지)
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+	char saveFilePath[100] = "C:\\Recordings\\output.mp4";  // 저장할 파일 경로
+
+	thread windowThread(showWindow);
+	thread keyThread(checkKey, playHandle, saveFilePath);
+
+	windowThread.join();
+	keyThread.join();
 
 	//---------------------------------------
-	// 라이브 스트리밍 종료
-	NET_DVR_StopRealPlay(playHandle);
-	NET_DVR_Logout(lUserID);
-	NET_DVR_Cleanup();
+	//라이브 스트리밍 종료
+
+	NET_DVR_StopRealPlay(playHandle); //라이브 뷰 종료
+	NET_DVR_Logout(cam.getIUserID()); //로그아웃
+	NET_DVR_Cleanup(); //SDK 종료
 
 	return 0;
 }
